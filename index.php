69f643b15362d3b6cc162fd366b8c88203ea1ea1
Subkha0506 <subhamuhammad95@gmail.com>
2024-10-31 23:38:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Me</title>
+  <link rel="stylesheet" href="style.css">
+
+  <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <script src="scripr.js"></script>
+
+  <main class="main">
+    <aside class="sidebar">
+      <nav class="nav">
+        <ul>
+          <li class="active"><a href="#">Welcome</a></li>
+          <li><a href="portofolio.html">Portofolio</a></li>
+          <li><a href="Riwayat Hidup.html">Riwayat Hidup</a></li>
+          <li><a href="Medsos.html">Media Sosial </a></li>
+        </ul>
+      </nav>
+    </aside>
+
+    <section class="twitter">
+      <div class="container">
+        <a target="_blank" href=""> </a>
+        <p>HISTORi HIDUP
+        <h1>Muhammad Subkha</h1>
+        <center>Website ini saya ciptakan untuk<br> mendeskripsikan pengalaman / riwayat<br> hidup saya.</center>
+        </p>
+
+      </div>
+    </section>
+  </main>
+</body>
+
+</html>
